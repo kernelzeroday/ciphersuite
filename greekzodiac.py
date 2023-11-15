c2551aa5bcc3fb637fbb4994233c1ae31a14c169
kelsey <enktal@gmail.com>
2023-11-15 12:32:08 -0800

    greekzodiac

---
+++ greekzodiac.py
@@ -0,0 +1,50 @@
+import argparse
+
+def custom_rot_cipher(input_str, shift, mode, null_symbol=None):
+    # English alphabet + Zodiac symbols + Greek symbols
+    custom_alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ♈♉♊♋♌♍♎♏♐♑♒♓ΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩ'
+    result = ''
+
+    # Adjust shift for decoding
+    if mode == 'decode':
+        shift = -shift
+
+    for char in input_str:
+        if char == null_symbol:
+            # Ignore the null symbol
+            continue
+
+        if char.upper() in custom_alphabet:
+            # Find the index in the custom alphabet (considering case)
+            is_uppercase = char.isupper()
+            index = custom_alphabet.find(char.upper())
+
+            # Shift the index by the specified shift value
+            shifted_index = (index + shift) % len(custom_alphabet)
+
+            # Add the shifted character to the result
+            if is_uppercase:
+                result += custom_alphabet[shifted_index]
+            else:
+                result += custom_alphabet[shifted_index].lower()
+        else:
+            # Leave characters not in the custom alphabet unchanged
+            result += char
+
+    return result
+
+def main():
+    parser = argparse.ArgumentParser(description='Custom ROT cipher with Zodiac and Greek symbols.')
+    parser.add_argument('mode', type=str, choices=['encode', 'decode'], help='Mode of operation: encode or decode.')
+    parser.add_argument('shift', type=int, help='Shift value for the cipher.')
+    parser.add_argument('message', type=str, help='Message to encode or decode.')
+    parser.add_argument('--null_symbol', type=str, default=None, help='Optional null symbol to be ignored in the cipher.')
+
+    args = parser.parse_args()
+
+    transformed_message = custom_rot_cipher(args.message, args.shift, args.mode, args.null_symbol)
+    print(transformed_message)
+
+if __name__ == "__main__":
+    main()
+
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
-#cipher suite
+# cipher suite
 
 usage:
 
@@ -9,4 +9,17 @@ M eq xli ♋shmeg Omppiv
 
 $ python zodiac.py decode 4 "M eq xli ♋shmeg Omppiv"
 I am the zodiac Killer
+
+
+
+
+
+
+$ python greekzodiac.py encode 23 "I am the zodiac killer"
+♍ x♑ ε♌♉ λ♓♈♍xz ♏♍♐♐♉γ
+
+
+$ python greekzodiac.py decode 23 "♍ x♑ ε♌♉♍ x♑ ε♌♉ λ♓♈♍xz ♏♍♐♐♉γ"
+i am the zodiac killer
+
 ```
